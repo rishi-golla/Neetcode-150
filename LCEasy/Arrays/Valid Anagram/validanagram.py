22fe70a96720d4c38829cc783c71e2c3eb496448
rishi-golla <rishigolla.123@gmail.com>
2026-01-22 15:09:36 -0600

    validanagram

--- LCEasy/Arrays/Valid Anagram/validanagram.py
+++ LCEasy/Arrays/Valid Anagram/validanagram.py
@@ -8,4 +8,10 @@ An anagram is a string that contains the exact same characters as another string
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-      
+        if sorted(s) == sorted(t):
+            return True
+        else:
+          return False
+        
+
+# Sort the char instead of checking the length, makes it much better for an anagram.
